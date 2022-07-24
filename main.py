d1c3480367bf034380c80a8a734b832e86884f0c
Andrew Peterson <andrewp2@umbc.edu>
2022-07-24 19:49:19 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# This program is going to be asking the user what items they want to produce and them
+
+
+item = input("What item do you need to make? ")
+amount = input("How many of those do you need to make? ")
+
+print(f"You said that you would like to make {amount} of: {item}. ")
+print(f"Calculating which resources are needed to create the {amount} {item}(s)....")
